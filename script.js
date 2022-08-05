06a03a26b529af0a01d5558682ef63de8e6c5308
leluka-le <lolatatunashvili@gmail.com>
2022-08-05 21:54:19 +0400

    Js-lesson6

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 //js-lesson6
 
-let buttons = document.querySelectorAll('.submit');
+//let buttons = document.querySelectorAll('.submit');
 
-buttons.forEach(function(item){
-item.addEventListener('click',function(){
-    alert ('hello');
-})
-})
+//buttons.forEach(function(item){
+//item.addEventListener('click',function(){
+    //alert ('hello');
+//})
+//})
+
+//let wraper = document.querySelector('.wraper');
+//function oneclickbutton(){
+    //alert ('Hello');
+//}
+
+//wraper.addEventListener('click',oneclickbutton);
